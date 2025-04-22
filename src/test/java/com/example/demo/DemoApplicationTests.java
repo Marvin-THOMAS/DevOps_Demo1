d8d2c900ba64cd942aa8e468ecb7f380ee516be0
Nyak4577 <marvin.thomas.4577@gmail.com>
2025-04-22 14:15:17 +0200

    TU

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	void test(){
+		final boolean myBoolean = false;
+		Assertions.assertFalse(myBoolean);
 	}
+
 
 }
